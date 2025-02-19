d4e5dcdd4ec21f9d1c8210723a8538ae8c40252f
PriyansuMaurya <priyanshu1999pm@gmail.com>
2025-02-19 20:07:56 +0530

    learned about multi-environement -  workspaces and file structure

--- 06-organization-and-modules/consul/main.tf
+++ 06-organization-and-modules/consul/main.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 2.70.0"
     }
   }
 }
@@ -31,6 +31,24 @@ provider "aws" {
 ## REPO: https://github.com/hashicorp/terraform-aws-consul
 ##
 ############################################################
-module "consul" {
-  source = "git@github.com:hashicorp/terraform-aws-consul.git"
+module "consul-starter" {
+  source  = "hashicorp/consul-starter/aws"
+  version = "0.2.0"
+
+  # Required Variables
+  allowed_inbound_cidrs = ["10.0.0.0/16"]  # Adjust based on your network
+  consul_version        = "1.14.4"         # Latest stable version, update if needed
+  name_prefix           = "consul-cluster" # Meaningful prefix
+  owner                 = "admin-user"     # Replace with your name or team name
+  vpc_id                = "vpc-0a1b2c3d4e" # Use your actual VPC ID
+
+  # Optional Variables (Modify if needed)
+  acl_bootstrap_bool     = true
+  consul_clients         = "3"
+  consul_cluster_version = "0.0.1"
+  consul_servers         = "5"
+  enable_connect         = false
+  instance_type          = "t3.medium"   # More cost-effective than "m5.large"
+  key_name               = "my-key-pair" # Replace with your SSH key name
+  public_ip              = false
 }
